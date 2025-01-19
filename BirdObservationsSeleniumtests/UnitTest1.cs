2a98e92a93f42d1e3381ff7a5c3458633c77c929
victor <victor.olszowski@gmail.com>
2025-01-19 20:34:39 +0100

    Added add and delete selenium tests

--- BirdObservationsSeleniumtests/UnitTest1.cs
+++ BirdObservationsSeleniumtests/UnitTest1.cs
@@ -1,6 +1,8 @@
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Support.UI;
+using System.Collections.ObjectModel;
 
 
 
@@ -17,11 +19,41 @@ namespace BirdObservationsSeleniumtests
             _driver = new ChromeDriver(DriverDirectory);
            
         }
+
+        [ClassCleanup]
+        public static void TearDown()
+        {
+            _driver.Dispose();
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
             _driver.Navigate().GoToUrl("file:///C:/Users/victor/Desktop/EksamensaetE/WebappEksamenSaetE/Index.html");
             Assert.AreEqual("Bird Observations", _driver.Title);
+
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+
+            IWebElement inputElement = _driver.FindElement(By.Id("inputName"));
+            inputElement.SendKeys("TestBird");
+            IWebElement inputElement2 = _driver.FindElement(By.Id("inputHowMany"));
+            inputElement2.SendKeys("1337");
+            IWebElement buttonCreateElement = _driver.FindElement(By.Id("createButton"));
+            buttonCreateElement.Click();
+            IWebElement outputMessage = wait.Until(x => x.FindElement(By.Id("addMessage")));
+            Assert.AreEqual("response 201 Created", outputMessage.Text);
+
+            ReadOnlyCollection<IWebElement> listElements2 = _driver.FindElements(By.TagName("tr"));
+
+            // Test af DELETE
+            IWebElement deleteInput = _driver.FindElement(By.Id("deleteInput"));
+            deleteInput.SendKeys((listElements2[5]).Text.Split(" ")[0]);
+            IWebElement deleteButton = _driver.FindElement(By.Id("deleteButton"));
+            deleteButton.Click();
+            IWebElement deleteMessage = wait.Until(x => x.FindElement(By.Id("deleteMessage")));
+            Assert.AreEqual("200 OK", deleteMessage.Text);
+
+
         }
     }
 }
